56d4664591bc6e4accac678ab72ac6f887496a85
Liz Dalton <elizabeth.dalton@onthebeach.co.uk>
2021-04-20 00:12:47 +0100

    Refactor the code be removing the contentCheck into its own helper file
    To remove some of the logic out of the index file I have moved the contentCheck
    into its own helper file and added a test to cover the logic in the file.

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
+var checkContent = require('./helpers/checkPasswordContent.js');
+
 const verify = (string) => {
   if (string == null) {
     throw new TypeError('You must create a password')
   } else if (string.length < 8) {
     throw new TypeError('Password is too short')
-  } else if (!checkContent(string).includes('upperCase')) {
+  } else if (!checkContent.verify(string).includes('upperCase')) {
     throw new TypeError('You must have at least 1 uppercase letter')
-  } else if (!checkContent(string).includes('number')) {
+  } else if (!checkContent.verify(string).includes('number')) {
     throw new TypeError('You must have at least 1 number')
-  } else if (!checkContent(string).includes('lowerCase')) {
+  } else if (!checkContent.verify(string).includes('lowerCase')) {
     throw new TypeError('You must have at least 1 lowercase letter')
   } else {
     return true
-  }
-}
-
-const checkContent = (string) => {
-  const chars = string.split('');
-  var type = [];
-  for (var i = 0; i < chars.length; i++) {
-    type.push(charType(chars[i]));
-  };
-  return type
-}
-
-const charType = (char) => {
-  if (!isNaN(char * 1)) {
-    return 'number'
-  } else if (char == char.toUpperCase()) {
-    return 'upperCase'
-  } else if (char == char.toLowerCase()) {
-    return 'lowerCase'
   }
 }
 
---
+++ spec/checkPasswordContentSpec.js
@@ -0,0 +1,15 @@
+var checkContent = require('../helpers/checkPasswordContent.js');
+
+describe("Test the Verify object", function() {
+  it('returns the unique contents.', function() {
+    expect(checkContent.verify('bigSmellyDog9')).toEqual(
+      ['lowerCase', 'upperCase', 'number']
+    );
+  });
+
+  it('returns the unique contents.', function() {
+    expect(checkContent.verify('bigsmellydog9')).toEqual(
+      ['lowerCase', 'number']
+    );
+  });
+});
